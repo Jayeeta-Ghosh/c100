5b4350700a43551bb352c7f2cefd261df42a2c11
Jayeeta-Ghosh <84728726+Jayeeta-Ghosh@users.noreply.github.com>
2022-06-11 21:05:21 +0530

    Add files via upload

---
+++ c100.py
@@ -0,0 +1,9 @@
+class Cat:
+    def __init__(self,color,size):
+        self.color=color
+        self.size=size
+    def Jayeeta(self):
+        print('colour:{}and size:{}'.format(self.color,self.size)) 
+
+cat1 = Cat('white',20)
+cat1.Jayeeta()
